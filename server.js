60a5d1928a625d2e4afa07191ab4e3fa05a64569
five <niip42@gmail.com>
2025-01-28 12:30:32 +0900

    Initial commits

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files like CSS
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the main landing page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
